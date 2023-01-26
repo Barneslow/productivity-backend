9b0fc5b0baa4a4f158c5c4810cd6e20df661d652
Darrach Barneveld <darrachbarneveld@Darrachs-MacBook-Pro.local>
2023-01-26 01:44:51 +0000

    updated vercel.json

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/emojis", emojiRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 app.listen(PORT, console.log(`Server is running on port ${PORT}`));
 
